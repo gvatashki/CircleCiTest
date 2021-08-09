430b63758351b5626ede3bf6bf0d35644d264496
Georgi <gvatashki0907@gmail.com>
2021-08-09 11:38:42 -0400

    added one more digit in to the function

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 
-function sum (a,b) {
-return a + b;
+function sum (a,b,c) {
+return a + b + c;
 }
 
 module.exports = sum;
--- sketch.test.js
+++ sketch.test.js
@@ -3,6 +3,6 @@
 const { test, expect } = require('@jest/globals');
 const sum = require('./sketch');
 
-test('add 1 + 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(3);
+test('add 1 + 2 + 8 to equal 11', () => {
+    expect(sum(1,2,8)).toBe(11);
 })
